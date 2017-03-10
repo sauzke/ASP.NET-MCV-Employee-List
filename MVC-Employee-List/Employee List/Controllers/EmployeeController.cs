34421e630bbebd866ccef5ab02bd258e34b7b14c
Jimmy Wang <jimmywang2142@gmail.com>
2017-03-09 18:30:06 -0800

    DB and model

---
+++ MVC-Employee-List/Employee List/Controllers/EmployeeController.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Employee_List.Models;
+using System.IO;
+
+namespace Employee_List.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private EmployeeContext db = new EmployeeContext();
+
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View(db.Employees.ToList());
+        }
+
+        // GET: Employee/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employeeModel = db.Employees.Find(id);
+
+            byte[] img = employeeModel.image;
+            if (img != null)
+            {
+                string path = byteArrayToImage(img);
+                ViewBag.ImageString = "data:image/jpeg;base64," + path;
+            }
+
+            if (employeeModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employeeModel);
+        }      
+
+        // GET: Employee/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employee/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,name,title,start_date,end_date")] Employee employee, HttpPostedFileBase image)
+        {
+            if (ModelState.IsValid)
+            {
+                if (image != null && image.ContentLength > 0)
+                {
+                    byte[] img = imageToByteArray(image);
+
+                    if (img != null)
+                        employee.image = img;
+                    else
+                        employee.image = null;
+                }
+                else
+                {
+                    employee.image = null;
+                }
+
+                db.Employees.Add(employee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(employee);
+        }
+
+        // GET: Employee/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employee/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,name,title,start_date,end_date,image")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employee).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+
+        // GET: Employee/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employee/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Employee employee = db.Employees.Find(id);
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        private byte[] imageToByteArray(HttpPostedFileBase file)
+        {
+            if (file != null && file.ContentLength > 0)
+            {
+                string pic = System.IO.Path.GetFileName(file.FileName);
+                string path = System.IO.Path.Combine(
+                                       Server.MapPath("~/images/profile"), pic);
+
+                file.SaveAs(path);
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    file.InputStream.CopyTo(ms);
+                    byte[] array = ms.GetBuffer();
+                    return array;
+                }
+            }
+            return null;
+        }
+
+        private string byteArrayToImage(byte[] byteArrayIn)
+        {
+            string temp = Convert.ToBase64String(byteArrayIn, 0, byteArrayIn.Length);
+            return temp;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MVC-Employee-List/Employee List/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace Employee_List.Models
+{
+    public class Employee
+    {        
+        public int ID { get; set; }
+        public string name { get; set; }
+        public string title { get; set; }
+        public DateTime start_date { get;  set; }
+        public DateTime end_date { get; set; }
+        public byte[] image { get; set; }
+    }
+
+    public class EmployeeContext: DbContext
+    {
+        public EmployeeContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
